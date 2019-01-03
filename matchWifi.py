4817a47ea0c139d27a394df39964fad1daa73ce2
abigfatstone <abigfatstone@msn.com>
2019-01-03 09:26:52 +0800

    recall

--- matchWifi.py
+++ matchWifi.py
@@ -5,12 +5,12 @@ import argparse
 import multiprocessing
 from datetime import datetime 
 
-OFFSET_LEFT_TIME =  0
+OFFSET_LEFT_TIME =  300
 OFFSET_RIGHT_TIME =  300
-OFFSET_LEFT_LON =  0
-OFFSET_RIGHT_LON =  0.0001
-OFFSET_LEFT_LAT =  0
-OFFSET_RIGHT_LAT =  0.0001
+OFFSET_LEFT_LON =  0.0002
+OFFSET_RIGHT_LON =  0.0002
+OFFSET_LEFT_LAT =  0.0002
+OFFSET_RIGHT_LAT =  0.0002
 
 PARALLEL = 8
 CSV_PATH= '/data/csvFile/'
@@ -34,37 +34,47 @@ def matchWifi(inPrefix):
     conn = psycopg2.connect(database="wifi", user="gpadmin", password="dev", host="master", port="5432")
     cur = conn.cursor()
 
+    idCard = ''
+    dicMac = {}
     taskStartTime = datetime.now() 
     for line in idFile:
-        idRows = idRows + 1
-        if idRows % 100 == 0:
 
-            print('{} MACs done. cost:{}s'.format(idRows,(datetime.now() - taskStartTime).seconds))
+        idRows = idRows + 1
+        if idRows % 30 == 0:
+            print('{} IDs done. cost:{}s'.format(idRows,(datetime.now() - taskStartTime).seconds))
             break
 
-        mac = line.split(',')[0]
+        IDfromFile = line.split(',')[0]
         ct = int(line.split(',')[1])
         lon = float(line.split(',')[2])
         lat = float(line.split(',')[3])
 
-        if lon > 0:
-            sqlText =   "select mac from wifi_clean where capture_time >= {} and capture_time <= {} and lon >= \'{}\' "\
-                        "and lon <= \'{}\' and lat >= \'{}\' and lat <= \'{}\'".format(
-                        ct - OFFSET_LEFT_TIME,
-                        ct + OFFSET_RIGHT_TIME, 
-                        lon - OFFSET_LEFT_LON ,
-                        lon + OFFSET_RIGHT_LON ,
-                        lat - OFFSET_LEFT_LAT,
-                        lat + OFFSET_RIGHT_LAT)
-            cur.execute(sqlText);
-            # print(sqlText)
-            rows = cur.fetchall()        # all rows in table
-            for i in rows:
-                print(i)
+        sqlText =   "select mac from wifi_clean where capture_time >= {} and capture_time <= {} and lon >= \'{}\' "\
+                    "and lon <= \'{}\' and lat >= \'{}\' and lat <= \'{}\'".format(
+                    ct - OFFSET_LEFT_TIME,
+                    ct + OFFSET_RIGHT_TIME, 
+                    lon - OFFSET_LEFT_LON ,
+                    lon + OFFSET_RIGHT_LON ,
+                    lat - OFFSET_LEFT_LAT,
+                    lat + OFFSET_RIGHT_LAT)
+        cur.execute(sqlText);
+        # print(sqlText)
+        rows = cur.fetchall()        # all rows in table
 
-    # conn.commit()
-    # cur.close()
-    # conn.close()
+        if IDfromFile != idCard:
+            # sorted(dicMac.items(),key=lambda item:item[1])
+            print(dicMac)
+            idCard = IDfromFile
+            dicMac = {}    
+        for row in rows:
+            if row[0] in dicMac:
+                dicMac[row[0]] = dicMac[row[0]] + 1
+            else:
+                dicMac[row[0]] = 1    
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
     args = None
