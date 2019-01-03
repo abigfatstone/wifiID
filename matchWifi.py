e372a6e70b2ba0e91b2c41084d8045ad10e65cf2
abigfatstone <abigfatstone@msn.com>
2019-01-03 10:15:48 +0800

    v0.1

--- matchWifi.py
+++ matchWifi.py
@@ -19,10 +19,15 @@ def parseArgs(args):
     parser = argparse.ArgumentParser()
     globalArgs = parser.add_argument_group('Global options')
     globalArgs.add_argument('--prefix', type=str, default='0,1') 
+    globalArgs.add_argument('--maxrow', type=int, default=10) 
     return parser.parse_args(args)
 
-def getMaxMac(inDicMac):
-    print(inDicMac)
+def getMaxMac(inDicMac,idCard,inPrefix):
+    try:
+        maxKey = max(inDicMac,key=inDicMac.get)
+        print('id:{}\t,maxHitMac:{}\t,hitRecords:{}\ttaskID:{}'.format(idCard,maxKey,inDicMac[maxKey],inPrefix))
+    except:
+        pass
     # sorted(dicMac.items(),key=lambda item:item[1])
 
 def matchWifi(inPrefix):
@@ -43,11 +48,6 @@ def matchWifi(inPrefix):
     taskStartTime = datetime.now() 
     for line in idFile:
 
-        idRows = idRows + 1
-        if idRows % 30 == 0:
-            print('{} IDs done. cost:{}s'.format(idRows,(datetime.now() - taskStartTime).seconds))
-            break
-
         IDfromFile = line.split(',')[0]
         ct = int(line.split(',')[1])
         lon = float(line.split(',')[2])
@@ -66,7 +66,7 @@ def matchWifi(inPrefix):
         rows = cur.fetchall()        # all rows in table
 
         if IDfromFile != idCard:
-            getMaxMac(dicMac)
+            getMaxMac(dicMac,idCard,inPrefix)
             idCard = IDfromFile
             dicMac = {}    
         for row in rows:
@@ -74,8 +74,16 @@ def matchWifi(inPrefix):
                 dicMac[row[0]] = dicMac[row[0]] + 1
             else:
                 dicMac[row[0]] = 1    
-    getMaxMac(dicMac)
-    print('{} IDs done. cost:{}s'.format(idRows,(datetime.now() - taskStartTime).seconds))
+
+        idRows = idRows + 1
+        if idRows % 100 == 0:
+            print('{} IDs done. cost:{}s,\t taskID:{}'.format(idRows,(datetime.now() - taskStartTime).seconds,inPrefix))
+        
+        if idRows == args.maxrow:
+            break
+
+    getMaxMac(dicMac,idCard,inPrefix)
+    print('{} IDs done. cost:{}s,\t taskID:{}'.format(idRows,(datetime.now() - taskStartTime).seconds,inPrefix))
     conn.commit()
     cur.close()
     conn.close()
